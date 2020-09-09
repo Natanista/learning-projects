11813a3eb0fe892e30cf10d961af0011c2846251
natanista <gdaynatan@gmail.com>
2020-09-09 19:21:00 -0300

    JS pratice

---
+++ 1 hour JS ytb class/index.js
@@ -0,0 +1,23 @@
+ // This is my first js code
+//console.log('Hello World');
+
+let name = 'Natan'; // STring Literal
+let age = 21; //Number Literal
+let isApproved = true; 
+//console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number(1name)
+// Cannot contain a space or hypen
+//Are case-sensitive
+
+
+//const interestRate = 0.3;
+//interestRate = 1;
+//console.log(interestRate);
+
+//Primitives: String, Number, Boolean, undefined, null
+
+
+
