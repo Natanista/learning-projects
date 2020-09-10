9fc6c6ebf4828aea38f362b2f7c29cf5746159cb
natanista <gdaynatan@gmail.com>
2020-09-09 21:02:27 -0300

    js class finished

--- 1 hour JS ytb class/index.js
+++ 1 hour JS ytb class/index.js
@@ -1,9 +1,13 @@
  // This is my first js code
 //console.log('Hello World');
 
-let name = 'Natan'; // STring Literal
-let age = 21; //Number Literal
-let isApproved = true; 
+//let name = 'Natan'; // STring Literal
+//let age = 21; //Number Literal
+//let isApproved = true;// Bolean Literal
+//let firstName = undefined;
+//let selectedColor = null;
+
+
 //console.log(name);
 
 // Cannot be a reserved keyword
@@ -13,11 +17,50 @@ let isApproved = true;
 //Are case-sensitive
 
 
-//const interestRate = 0.3;
-//interestRate = 1;
+// const interestRate = 0.3; if u don't wanna change in the future use cons
+// interestRate = 1;
 //console.log(interestRate);
 
 //Primitives: String, Number, Boolean, undefined, null
+
+
+let person = {
+    name: 'Natan',
+    age: 21,
+};
+
+// Dot Notation
+person.name = 'Neitan'
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name);
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+
+
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello, ' + name + ' ' + lastName);
+}
+
+greet('Natan','Silva');
+
+// Calculatiing a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
+
+
+
 
 
 
