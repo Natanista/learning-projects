02c6a30f99fc8d699e4045e7c885c8504814e3ac
natanista <gdaynatan@gmail.com>
2020-09-15 19:31:47 -0300

    organization

--- sql projects/SQL-Course-Materials/mysql-learning.sql
+++ sql projects/SQL-Course-Materials/mysql-learning.sql
@@ -67,3 +67,10 @@ FROM orders
 JOIN customers 
 ON orders.customer_id = customers.customer_id;
 
+SELECT e.employee_id,
+e.first_name,
+m.first_name AS manager
+FROM employees e
+JOIN employees m 
+ON e.reports_to = m.employee_id;
+
