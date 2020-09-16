86a9187aad8f7468241378571ed7b46df09e3355
natanista <gdaynatan@gmail.com>
2020-09-16 00:16:27 -0300

    adicionando arquivo JS no projeto SIGNOS

---
+++ signos/index.js
@@ -0,0 +1,47 @@
+function btn1() {
+    resposta.innerHTML = `<br> Coldplay - Adventure Of A Lifetime `
+}
+
+function btn2() {
+    resposta.innerHTML = `<br>Anthrax - Got The Time `
+}
+
+function btn3() {
+    resposta.innerHTML = `<br>XXXTENTACION - changes `
+}
+
+function btn4() {
+    resposta.innerHTML = `<br>Lady Gaga, Ariana Grande - Rain On Me`
+}
+
+function btn5() {
+    resposta.innerHTML = `<br>Taylor Swift - Love Story `
+}
+
+function btn6() {
+    resposta.innerHTML = `<br>New Order - Blue Monday `
+}
+
+function btn7() {
+    resposta.innerHTML = `<br>Marina Peralta - Agradece `
+}
+
+function btn8() {
+    resposta.innerHTML = `<br>Burzum - Dunkelheit`
+}
+
+function btn9() {
+    resposta.innerHTML = `<br>Engenheiros Do Hawaii - 3ª Do Plural`
+}
+
+function btn10() {
+    resposta.innerHTML = `<br>Rihanna - Work ft. Drake`
+}
+
+function btn11() {
+    resposta.innerHTML = `<br>Weezer - Say It Ain't So`
+}
+
+function btn12() {
+    resposta.innerHTML = `<br>Daft Punk - One More Time`
+}
