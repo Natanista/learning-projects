4e677e5d3057b45565a005b4bce76cf41bf3fd28
natanista <gdaynatan@gmail.com>
2020-10-25 00:59:03 -0300

    aula de js

--- javascript/condition.js
+++ javascript/condition.js
@@ -1,1 +1,8 @@
-console.log('O console funcionou')
+var velocidade = 60.4;
+
+if (velocidade > 60) {
+  console.log("Você ultrapassou a velocidade permitida!MULTADO");
+} else {
+  console.log(`A velocidade do seu carro é de ${velocidade}Km/h`);
+  console.log("Dirija sempre usando cinto");
+}
