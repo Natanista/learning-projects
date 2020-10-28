a6290a10907a64319e683d542da2ebf10315c751
natanista <gdaynatan@gmail.com>
2020-10-28 12:02:06 -0300

    verificador de idade

--- javascript/ex002/ex002.js
+++ javascript/ex002/ex002.js
@@ -9,12 +9,34 @@ function verificar() {
     var fsex = document.getElementsByName("radsex");
     var idade = ano - Number(fano.value);
     var genero;
+    var img = document.createElement('img');
+    img.setAttribute('id', 'foto');
+
     if (fsex[0].checked) {
       genero = "Homem";
+      if(idade >= 0 && idade < 10 ){
+        img.setAttribute('src','img/crianca_menino.png')
+      }else if(idade < 21){
+        img.setAttribute('src','img/adolescente_homem.png')
+      }else if(idade < 50){
+        img.setAttribute('src','img/adulto_homem.png')
+      }else{
+        img.setAttribute('src','img/idoso_homem.png')
+      }
     } else if (fsex[1].checked) {
       genero = "Mulher";
+      if(idade >= 0  && idade < 10 ){
+        img.setAttribute('src','img/crianca_menina.png')
+      }else if(idade < 21){
+        img.setAttribute('src','img/adolescente_mulher.png')
+      }else if(idade < 50){
+        img.setAttribute('src','img/adulto_mulher.png')
+      }else{
+        img.setAttribute('src','img/idoso_mulher.png')
+      }
     }
-    res.style.textAlign = 'center';
+    res.style.textAlign = "center";
     res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+    res.appendChild(img)
   }
 }
