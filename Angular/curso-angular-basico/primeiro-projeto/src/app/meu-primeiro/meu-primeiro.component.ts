eb725303c769e6a3f835ced6590bea7c50ca521f
natanista <natan.silva@bandtec.com.br>
2021-04-23 14:15:33 -0300

    finalizacao curso de angular

--- Angular/curso-angular-basico/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'meu-primeiro-component',
-    template: `
-    <p>Meu primeiro component HAHA :D</p>
-    `
-})
-export class MeuPrimeiroComponent{}
